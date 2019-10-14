f9623ddec8f4941b39d5b5ec0b5817083831de37
pablo <pablopaparini@hotmail.com>
2019-10-14 09:05:33 -0300

    test

--- src/test/java/com/boiler/BoilerplateApplicationTests.java
+++ src/test/java/com/boiler/BoilerplateApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import com.boiler.config.security.JwtTokenMockUtil;
 import com.boiler.services.PeopleService;
 
